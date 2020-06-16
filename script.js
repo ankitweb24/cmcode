56c553b16cb4d7e7feda577ede2a1f1e7d37fc96
Ankit Jangir <61105805+ankitweb24@users.noreply.github.com>
2020-06-16 10:28:09 -0700

    canncode
    please publish this website

---
+++ script.js
@@ -0,0 +1,12 @@
+var type = new Typed('#typed', {
+    stringsElement: '#items',
+    typeSpeed: 100,
+    startDelay: 1200,
+    backSpeed: 30,
+    loop: true,
+    loopCount: 1000
+});
+$(".count").counterUp({
+    delay: 100,
+    time: 8000
+})
